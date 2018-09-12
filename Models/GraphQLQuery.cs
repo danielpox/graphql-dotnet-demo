8b1e1fe2fbc8656e5c06370a0bebd0f8882bb550
Daniel Hallgren <daniel.hallgren@cdab.eu>
2018-09-12 15:21:37 +0200

    Add query request object

---
+++ Models/GraphQLQuery.cs
@@ -0,0 +1,10 @@
+namespace eu.cdab.GraphQL_Gokhan.Models
+{
+    public class GraphQLQuery
+    {
+        public string OperationName { get; set; }
+        public string NamedQuery { get; set; }
+        public string Query { get; set; }
+        public string Variables { get; set; }
+    }
+}
