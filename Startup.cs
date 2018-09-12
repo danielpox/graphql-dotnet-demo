ead5f72a7caf1187ba28508bc5652c11a23c0b75
Daniel Hallgren <daniel.hallgren@cdab.eu>
2018-09-12 15:34:57 +0200

    Add dependency injection

--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-
-namespace www.gokhan_gokalp.com
+﻿namespace www.gokhan_gokalp.com
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.HttpsPolicy;
+    using Microsoft.AspNetCore.Mvc;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
+
+    using GraphQL;
+    using GraphQL.Types;
+
+    using eu.cdab.GraphQL_Gokhan.Data;
+    using eu.cdab.GraphQL_Gokhan.Models;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -26,6 +34,20 @@ namespace www.gokhan_gokalp.com
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddScoped<EasyStoreQuery>();
+
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IProductRepository, ProductRepository>();
+
+            services.AddScoped<IDocumentExecuter, DocumentExecuter>();
+
+            services.AddTransient<CategoryType>();
+            services.AddTransient<ProductType>();
+
+            var sp = services.BuildServiceProvider();
+            
+            services.AddScoped<ISchema>(_ => new EasyStoreSchema(type => (GraphType) sp.GetService(type)) { Query = sp.GetService<EasyStoreQuery>() });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
